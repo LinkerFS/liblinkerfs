8228128d6d2e801fb05f5e897f74464360d08980
kaedeair <34467508+kaedeair@users.noreply.github.com>
2024-06-04 12:39:47 +0800

    Revert "feat(warp_config): use pointer array instead of a data array for warp targets"
    This reverts commit cf45f7cc5b329b581407c0b744b5003eb4080f63.

--- src/data/warp.h
+++ src/data/warp.h
@@ -32,7 +32,7 @@ typedef struct wrap_target {
 } WARP_TARGET;
 
 typedef struct wrap_config {
-    WARP_TARGET **warp_targets_ptr;
+    WARP_TARGET *warp_targets;
     int warp_count;
 } WARP_CONFIG;
 #endif //LIBLINKERFS_WARP_H
